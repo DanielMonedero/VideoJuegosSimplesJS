b32216c8860900b1bbf6745eae54c9ac6c694b12
Guillermo Callizaya Fernandez <118977401+Wyllop@users.noreply.github.com>
2023-04-16 16:37:43 +0200

    Version final revisada

--- Tresenraya/tresenraya.js
+++ Tresenraya/tresenraya.js
@@ -1,32 +1,35 @@
+/* Se define una constante 'cells' que contiene todos los elementos HTML que tienen la clase 'cell' */
 const cells = document.querySelectorAll(".cell");
 let currentPlayer = "X";
 
+/* Se añade un evento 'dragover' y 'drop' a cada celda en 'cells' mediante un bucle forEach */
 cells.forEach((cell) => {
   cell.addEventListener("dragover", dragOver);
   cell.addEventListener("drop", dragDrop);
 });
-
+/* Se define la función 'dragStart' que se ejecuta cuando comienza el arrastre de un elemento */
 function dragStart(event) {
   event.dataTransfer.setData("text", event.target.id);
 }
-
+/* Se define la función 'dragOver' que se ejecuta cuando un elemento se arrastra sobre una celda */
 function dragOver(event) {
   event.preventDefault();
 }
-
+/* Se define la función 'dragDrop' que se ejecuta cuando se suelta un elemento arrastrado sobre una celda */
 function dragDrop(event) {
   const cell = event.target;
   const data = event.dataTransfer.getData("text");
-
+/* Si la celda no tiene contenido, se añade el contenido de 'currentPlayer' y se cambia 'currentPlayer' a "O" si era "X" o viceversa */
   if (cell.textContent === "") {
     cell.textContent = currentPlayer;
     currentPlayer = currentPlayer === "X" ? "O" : "X";
   }
-
+/* Se comprueba si hay un ganador */
   checkWinner();
 }
-
+/* Se define la función 'checkWinner' que comprueba si hay un ganador */
 function checkWinner() {
+  /* Se define un array con todas las combinaciones ganadoras */
   const winningCombinations = [
     [0, 1, 2],
     [3, 4, 5],
@@ -40,7 +43,7 @@ function checkWinner() {
 
   for (let i = 0; i < winningCombinations.length; i++) {
     const [a, b, c] = winningCombinations[i];
-
+/* Si las celdas 'a', 'b' y 'c' tienen el mismo contenido, se muestra un mensaje de alerta con el jugador ganador y se reinicia el juego */
     if (
       cells[a].textContent &&
       cells[a].textContent === cells[b].textContent &&
@@ -50,13 +53,13 @@ function checkWinner() {
       resetGame();
     }
   }
-
+/* Si todas las celdas tienen contenido y no hay ganador, se muestra un mensaje de alerta de empate y se reinicia el juego */
   if ([...cells].every((cell) => cell.textContent !== "")) {
-    alert("It's a tie!");
+    alert("¡¡EMPATE!!");
     resetGame();
   }
 }
-
+/* Se define la función 'resetGame' que reinicia el juego, vaciando todas las celdas y estableciendo 'currentPlayer' a "X" */
 function resetGame() {
   cells.forEach((cell) => {
     cell.textContent = "";
--- pagPrincipal/pagPrincipalJS.js
+++ pagPrincipal/pagPrincipalJS.js
@@ -1,9 +1,11 @@
+//Obtenemos los botones correspondientes a cada juego//
 const juego1Button = document.getElementById("juego1");
 const juego2Button = document.getElementById("juego2");
 const juego3Button = document.getElementById("juego3");
 const juego4Button = document.getElementById("juego4");
 const randomButton = document.getElementById("random");
 
+//Asignamos un evento de click para redirigir a la página de cada juego//
 juego1Button.addEventListener("click", () => {
   window.location.href = "../quiz/quiz.html";
 });
@@ -19,9 +21,9 @@ juego3Button.addEventListener("click", () => {
 juego4Button.addEventListener("click", () => {
   window.location.href = "../Tresenraya/tresenraya.html";
 });
-
+//Asignamos un evento de click para redirigir a una página aleatoria de juego//
 randomButton.addEventListener("click", () => {
-  const juegos = ["../quiz/quiz.html", "../simonDice/simonDice.html", "../ahorcado/ahorcado.html"];
+  const juegos = ["../quiz/quiz.html", "../simonDice/simonDice.html", "../ahorcado/ahorcado.html","../Tresenraya/tresenraya.html"];
   const randomIndex = Math.floor(Math.random() * juegos.length);
   window.location.href = juegos[randomIndex];
 });
