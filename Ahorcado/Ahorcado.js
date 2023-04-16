8f1b5030d8da2816a05b3cf9d9e8c03e5750e350
Daniel Monedero Cabrera <115783761+tryhardPelusa@users.noreply.github.com>
2023-04-16 20:31:29 +0200

    Bugs tres en raya y ahorcado arreglados

---
+++ Ahorcado/Ahorcado.js
@@ -0,0 +1,121 @@
+/*ponemos todas la varables que vamos a necesitar*/
+var resultElem = {},
+    currentStatus = {},
+    selectedWord = '',
+    numberOfTries = 0,
+    numberOfFound = 0,
+    totalRights = 7;
+var buttonFragment;
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+/* Esta función comprueba si la letra seleccionada por el usuario coincide con alguna de las letras de la palabra seleccionada */
+function check() {
+    var currentValue = this.textContent,
+        found = false;
+    /* Este bucle compara cada letra de la palabra seleccionada con la letra seleccionada por el usuario */
+    for (var index = 0, length = selectedWord.length; index < length; index++) {
+        /* Si se encuentra una letra, se muestra en el campo correspondiente */
+        if (selectedWord[index] == currentValue) {
+            document.getElementsByTagName('input')[index].value = currentValue;
+            found = true;
+            /* Se incrementa el número de letras encontradas */
+            numberOfFound++;
+        }
+    }
+    /* Si se han encontrado todas las letras, se indica que el usuario ha ganado y se desactivan los botones */
+    if (numberOfFound == selectedWord.length) {
+        resultElem.textContent = '¡¡¡HAS GANADO!!!';
+        return disableButtons(document.getElementsByTagName('buttons'));
+    }
+    /* Si no se ha encontrado la letra seleccionada, se desactiva el botón correspondiente y se actualiza el estado del juego. Si se han agotado los intentos, se indica que se ha perdido y se reincia.*/
+    this.disabled = true;
+
+    if (found === false) {
+        numberOfTries++;
+        currentStatus.src = 'status_' + numberOfTries + '.gif';
+
+        var resultText = 'Has fallado ' + numberOfTries + ' '
+        resultText += '<br />';
+        resultText += 'Tienes ' + (totalRights - numberOfTries) + ' intentos.';
+        resultElem.innerHTML = resultText;
+        // Si el total de fallos coincide con los fallos máximos lo advertimos y reiniciamos el juego
+        if (numberOfTries == totalRights) {
+            // Ambos eventos tienen un timeout para que de tiempo a terminar el muñeco y que salga alerta sin reinciar el juego
+            setTimeout(() =>  alert("Has perdido :("), 200);
+            setTimeout(() => restartGame(), 1000);
+        }
+    }
+}
+/* Esta función desactiva los botones pasados como argumento. */
+function disableButtons(buttons) {
+    for (var index = 0, length = buttons.length; index < length; index++) {
+        console.log("Entramos en desactivar botones");
+        buttons[index].disabled = true;
+    }
+}
+/* Esta función inicializa los elementos necesarios para el juego, como la palabra y los campos de entrada y botones. */
+function initElement() {
+    resultElem = document.getElementById('results');
+    currentStatus = document.getElementById('current-status');
+    // Inicializar variables de alfabeto y palabras
+    var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    var words = ['lenguaje', 'anacleto', 'tigre', 'esternon', 'codigofacilito', 'inteligencia', 'badbunny'];
+    var position = randomNumber(0, words.length);
+    selectedWord = words[position].toUpperCase();
+    // Crear campos de entrada
+    var inputFragment = document.createDocumentFragment();
+    for (var index = 0, length = selectedWord.length; index < length; index++) {
+        var newInput = document.createElement('input');
+        newInput.type = 'text';
+        newInput.maxLength = 1;
+        newInput.readOnly = true;
+        inputFragment.appendChild(newInput);
+    }
+    document.getElementById('inputs').appendChild(inputFragment);
+    // Crear botones
+    buttonFragment = document.createDocumentFragment();
+    for (var index = 0; index < alphabet.length; index++) {
+        var newButton = document.createElement('button');
+        newButton.textContent = alphabet[index];
+        newButton.addEventListener('click', check);
+        buttonFragment.appendChild(newButton);
+    }
+    document.getElementById('buttons').appendChild(buttonFragment);
+
+}
+
+function init() {
+    initElement();
+    document.getElementById('restart-btn').addEventListener('click', restartGame);
+}
+function restartGame() {
+    // Reiniciar resultados
+    resultElem.textContent = '';
+    resultElem.innerHTML = '';
+    currentStatus.src = 'status_0.gif';
+
+    // Habilitar botones de letras nuevamente
+    var buttons = document.querySelectorAll('#buttons button');
+    buttons.forEach(function (button) {
+        button.disabled = false;
+    });
+
+    // Reiniciar variables
+    selectedWord = '';
+    numberOfTries = 0;
+    numberOfFound = 0;
+
+    // Crear una nueva palabra y campos de entrada
+    var inputFields = document.getElementById('inputs');
+    inputFields.innerHTML = '';
+    var buttonFields = document.getElementById('buttons');
+    buttonFields.innerHTML = '';
+
+    initElement();
+}
+
+
+
+window.addEventListener('DOMContentLoaded', init);
---
+++ Tresenraya/tresenraya.js
@@ -0,0 +1,76 @@
+/* Se define una constante 'cells' que contiene todos los elementos HTML que tienen la clase 'cell' */
+const cells = document.querySelectorAll(".cell");
+let currentPlayer = "X";
+let piezaArrastable = document.getElementById("arrastrable");
+piezaArrastable.innerHTML = `Turno X`;
+
+/* Se añade un evento 'dragover' y 'drop' a cada celda en 'cells' mediante un bucle forEach */
+cells.forEach((cell) => {
+  cell.addEventListener("dragover", dragOver);
+  cell.addEventListener("drop", dragDrop);
+});
+/* Se define la función 'dragStart' que se ejecuta cuando comienza el arrastre de un elemento */
+function dragStart(event) {
+  event.dataTransfer.setData("text", event.target.id);
+}
+/* Se define la función 'dragOver' que se ejecuta cuando un elemento se arrastra sobre una celda */
+function dragOver(event) {
+  event.preventDefault();
+}
+/* Se define la función 'dragDrop' que se ejecuta cuando se suelta un elemento arrastrado sobre una celda */
+function dragDrop(event) {
+  const cell = event.target;
+  const data = event.dataTransfer.getData("text");
+/* Si la celda no tiene contenido, se añade el contenido de 'currentPlayer' y se cambia 'currentPlayer' a "O" si era "X" o viceversa */
+  if (cell.textContent === "") {
+    cell.textContent = currentPlayer;
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    currentPlayer === "X" ? piezaArrastable.innerHTML = `Turno X` : piezaArrastable.innerHTML = `Turno O`;
+  }
+/* Se comprueba si hay un ganador */
+  checkWinner();
+}
+/* Se define la función 'checkWinner' que comprueba si hay un ganador */
+function checkWinner() {
+  /* Se define un array con todas las combinaciones ganadoras */
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+/* Si las celdas 'a', 'b' y 'c' tienen el mismo contenido, se muestra un mensaje de alerta con el jugador ganador y se reinicia el juego */
+    if (
+      cells[a].textContent &&
+      cells[a].textContent === cells[b].textContent &&
+      cells[b].textContent === cells[c].textContent
+    ) {
+      // He añadido un timeOut para que de tiempo a ver el tablero y no se borre nada mas soltar la pieza ganadora
+      setTimeout(() => alert(`¡Jugador ${cells[a].textContent} gana!`), 500);
+      setTimeout(() => resetGame(), 500);
+      
+    }
+  }
+/* Si todas las celdas tienen contenido y no hay ganador, se muestra un mensaje de alerta de empate y se reinicia el juego */
+  if ([...cells].every((cell) => cell.textContent !== "")) {
+    // He añadido un timeOut para que de tiempo a ver el tablero completo y no se borre nada mas soltar la ultima pieza
+    setTimeout(() => alert("¡¡EMPATE!!"), 500);
+    setTimeout(() => resetGame(), 500);
+    
+  }
+}
+/* Se define la función 'resetGame' que reinicia el juego, vaciando todas las celdas y estableciendo 'currentPlayer' a "X" */
+function resetGame() {
+  cells.forEach((cell) => {
+    cell.textContent = "";
+  });
+  currentPlayer = "X";
+  piezaArrastable.innerHTML = `Turno X`;
+}
